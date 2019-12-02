05fc4466fd411cc2555b1501616ec28e0133169a
liushuai <770722922@qq.com>
2019-12-03 01:14:43 +0800

    chore: rename USB to ScopeMCU

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -67,7 +67,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "Scope"
 #define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_PRODUCT_STRING_FS     "ScopeMCU"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
